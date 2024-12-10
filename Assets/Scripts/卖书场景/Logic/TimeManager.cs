8ae440d1958fb10dda63a46c839a54d5c3a4b692
Lyca-Lord <LYCALORD2023@outlook.com>
2024-12-10 22:34:47 +0800

    实现时间互通
    存在一个问题：UpdateTimeUI()函数必须存在订阅才能成功调用，但是这个函数内部的invoke已经加了判空了

--- Assets/Scripts/卖书场景/Logic/TimeManager.cs
+++ Assets/Scripts/卖书场景/Logic/TimeManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class TimeManager : MonoBehaviour
 {
@@ -9,9 +11,21 @@ public class TimeManager : MonoBehaviour
     public int hour;
     public int minute;
 
+    private bool isCountingTime = false;
+    private float duration = 0;
+    private int mySpeed;
+
     private void Awake() {
-        if (Singleton != null) return;
+        if(Singleton != null&&Singleton != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         Singleton = this;
+        DontDestroyOnLoad(gameObject);
+        isCountingTime = false;
+        EventHandler.UpdateTimeUI += CheckTimer;
     }
 
     public void StartNewDay(int hour){
@@ -30,6 +44,8 @@ public class TimeManager : MonoBehaviour
     public IEnumerator StartNewPeriord(int start, int end, int speed){
         hour = start;
         minute = 0;
+        isCountingTime = true;
+        mySpeed = speed;
 
         //TODO:进入时触发
 
@@ -46,4 +62,24 @@ public class TimeManager : MonoBehaviour
 
         //TODO:退出时触发
     }
+
+    private void Update()
+    {
+        if (isCountingTime == false) return;
+        duration += Time.deltaTime;
+        if(duration >= mySpeed)
+        {
+            duration = 0;
+            minute += 10;
+            if (minute >= 60)
+            {
+                minute = minute % 60;
+                hour++;
+            }
+
+            EventHandler.UpdateTimeUI(hour, minute);
+        }
+    }
+
+    private void CheckTimer(int a,int b) { }//空函数，用来确保订阅非空
 }
