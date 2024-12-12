1d368b161bba41c0e6bdcaebae82fc63ed088d09
Lyca-Lord <LYCALORD2023@outlook.com>
2024-12-12 21:58:22 +0800

    fix

--- Assets/Scripts/大厅/HallButtonScript.cs
+++ Assets/Scripts/大厅/HallButtonScript.cs
@@ -6,17 +6,17 @@ using UnityEngine.UI;
 public class HallButtonScript : MonoBehaviour
 {
     [Header("´óÌü")]
-    [SerializeField] private Button buyButton;
+    [SerializeField] private Button sellButton;
     [SerializeField] private Button bedButton;
-    [SerializeField] private Button underButton;
+    [SerializeField] private Button buyButton;
 
     void Start()
     {
-        buyButton.onClick.AddListener(
+        sellButton.onClick.AddListener(
             () => { SceneLoadManager.Singleton.LoadScene("ÂôÊé³¡¾°"); });
         bedButton.onClick.AddListener(
             () => { SceneLoadManager.Singleton.LoadScene("-1"); });
-        underButton.onClick.AddListener(
+        buyButton.onClick.AddListener(
             () => { SceneLoadManager.Singleton.LoadScene("NightShop"); });
     }
 
